f55edbbc13212a2c9f042331e13347a4d0779582
Jahangir506 <zahangirkhan506@gmail.com>
2023-12-22 17:49:15 +0600

    create task delete api

--- index.js
+++ index.js
@@ -25,11 +25,63 @@ async function run() {
     // await client.connect();
 
     const taskCollection = client.db('job_task1').collection('task')
+    
 
    app.get('/task', async(req, res)=> {
     const result = await taskCollection.find().toArray()
     res.send(result)
    })
+
+   app.put('/update', async (req, res) => {
+    const id = req.query.id;
+    const filter = { _id: new ObjectId(id) };
+    const data = req.body;
+    const updatedDoc = {
+      $set: {
+        title: data.title,
+        description: data.description,
+        priority: data.priority,
+        deadline: data.deadline,
+        email: data.email,
+      },
+    };
+    const result = await taskCollection.updateOne(filter, updatedDoc);
+    res.send(result);
+  });
+
+  app.post('/task', async (req, res) => {
+    const taskadd = req.body;
+    const data = {
+      title: taskadd.title,
+      description: taskadd.description,
+      date: taskadd.date,
+      priority: taskadd.priority,
+      status: 'todo',
+    };
+    const result = await taskCollection.insertOne(data);
+    res.send(result);
+  });
+
+  app.patch('/status', async (req, res) => {
+    const id = req.query.id;
+    const data = req.body;
+    const query = { _id: new ObjectId(id) };
+    const updatedDoc = {
+      $set: {
+        status: data.status,
+      },
+    };
+    const result = await taskCollection.updateOne(query, updatedDoc);
+    res.send(result);
+  });
+
+  app.delete('/delete', async (req, res) => {
+    const id = req.query.id;
+    console.log(id);
+    const query = { _id: new ObjectId(id) };
+    const result = await taskCollection.deleteOne(query);
+    res.send(result);
+  });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
